825028d9368827d80efcefefc56ab85127afcf7d
Madhubhashini Abekoon <madhu.abekoon@gmail.com>
2019-07-11 07:11:10 +0530

    Add project files.

---
+++ ReverseStringConsoleApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReverseStringConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReverseString("tree");
+        }
+
+        internal static void ReverseString(string str)
+        {
+
+            char[] charArray = str.ToCharArray();
+            for (int i=0, j=str.Length-1; i<j; i++, j--)
+            {
+                charArray[i] = str[j];
+                charArray[j] = str[i];
+            }
+            string newArrayString = new string(charArray);
+            Console.WriteLine(newArrayString);
+        }
+    }
+}
